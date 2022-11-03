c71841b995f4a11cbc2ff212e9f133517d0abc87
Andrew Stocker <sleepyams@gmail.com>
2022-11-03 12:41:25 -0600

    output

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-mod app;
-mod sound;
-mod playback;
-mod ui;
-mod interpolator;
-mod utils;
 
+use std::sync::Arc;
 
 use assert_no_alloc::*;
 use crossbeam_channel::unbounded;
+use dasp::Sample;
 use hound;
 use cpal::{SampleRate, StreamConfig};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
+mod utils;
+mod interpolator;
+mod output;
 use interpolator::LinearInterpolator;
+use output::{MonoToStereo, StereoOutput};
 
 
 // assert_no_alloc
@@ -22,52 +22,25 @@ use interpolator::LinearInterpolator;
 static A: AllocDisabler = AllocDisabler;
 
 
-struct MonoToStereo<I> where I: Iterator {
-    iterator: I,
-    buffer: Option<I::Item>,
-    first: bool
-}
-
-impl<I> MonoToStereo<I> where I: Iterator {
-    pub fn new(iterator: I) -> Self {
-        Self {
-            iterator,
-            buffer: None,
-            first: true
-        }
-    }
-}
-
-impl<I> Iterator for MonoToStereo<I> where I: Iterator, I::Item: Copy {
-    type Item = I::Item;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.first {
-            self.buffer = self.iterator.next();
-            self.first = false;
-            self.buffer
-        } else {
-            self.first = true;
-            self.buffer
-        }
-    }
-}
-
-
 fn main() {
     let host = cpal::default_host();
     let device = host.default_output_device().unwrap();
     let config: StreamConfig = device.default_output_config().unwrap().into();
+    let num_channels = config.channels;
     let SampleRate(sample_rate) = config.sample_rate;
 
-    let wav = hound::WavReader::open("samples/kick.wav").unwrap();
+    let wav = hound::WavReader::open("samples/hihat.wav").unwrap();
     let spec = wav.spec();
 
-    let mut samples = MonoToStereo::new(
-        LinearInterpolator::new(
-            wav.into_samples::<i16>().map(|r| r.unwrap()),
-            (sample_rate as f64) / (spec.sample_rate as f64)
-        )
+    let samples = LinearInterpolator::new(
+        wav.into_samples::<i16>().map(|r| r.unwrap()),
+        (sample_rate as f64) / (spec.sample_rate as f64)
+    );
+
+    let mut output = StereoOutput::new(
+        MonoToStereo::new(samples),
+        num_channels,
+        (1, 2)
     );
 
     let (sender, receiver) = unbounded();
@@ -77,8 +50,8 @@ fn main() {
         move |data: &mut [f32], _| {
             // assert_no_alloc(|| {
                 for out_sample in data.iter_mut() {
-                    if let Some(sample) = samples.next() {
-                        *out_sample = cpal::Sample::from(&sample);
+                    if let Some(sample) = output.next() {
+                        *out_sample = sample.to_sample::<f32>();
                     } else {
                         sender.send(()).unwrap();
                     }
---
+++ src/output.rs
@@ -0,0 +1,77 @@
+use dasp::Sample;
+
+
+pub enum StereoChannel {
+    Left,
+    Right
+}
+
+pub struct MonoToStereo<I> where I: Iterator {
+    iterator: I,
+    buffer: Option<I::Item>,
+    first: bool
+}
+
+impl<I> MonoToStereo<I> where I: Iterator {
+    pub fn new(iterator: I) -> Self {
+        Self {
+            iterator,
+            buffer: None,
+            first: true
+        }
+    }
+}
+
+impl<I> Iterator for MonoToStereo<I> where I: Iterator, I::Item: Copy {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.first {
+            self.buffer = self.iterator.next();
+            self.first = false;
+            self.buffer
+        } else {
+            self.first = true;
+            self.buffer
+        }
+    }
+}
+
+
+pub struct StereoOutput<I> {
+    iterator: I,
+    num_channels: u16,
+    output_channels: (u16, u16),
+    current_channel: u16,
+    next_channel: StereoChannel
+}
+
+impl<I> StereoOutput<I> where I: Iterator {
+    pub fn new(iterator: I, num_channels: u16, output_channels: (u16, u16)) -> Self {
+        assert!(output_channels.0 != output_channels.1);
+        Self {
+            iterator,
+            num_channels,
+            output_channels,
+            current_channel: 0,
+            next_channel: StereoChannel::Left
+        }
+    }
+}
+
+impl<I> Iterator for StereoOutput<I> where I: Iterator, I::Item: Sample {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current_channel = (self.current_channel + 1) % self.num_channels;
+        if (self.current_channel == self.output_channels.0) || (self.current_channel == self.output_channels.1) {
+            self.next_channel = match self.next_channel {
+                StereoChannel::Left => StereoChannel::Right,
+                StereoChannel::Right => StereoChannel::Left
+            };
+            self.iterator.next()
+        } else {
+            Some(Self::Item::EQUILIBRIUM)
+        }
+    }
+}
